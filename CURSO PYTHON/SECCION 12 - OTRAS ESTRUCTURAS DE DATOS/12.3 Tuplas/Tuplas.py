a0792d75951330faadcfb43c12fd88898cd0d674
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:17:39 -0300

    Create Tuplas.py

---
+++ CURSO PYTHON/SECCION 12 - OTRAS ESTRUCTURAS DE DATOS/12.3 Tuplas/Tuplas.py
@@ -0,0 +1,8 @@
+tupla = 1, 2, 3, 4, 5
+tupla2 = 6, 7, 8, 9, 10
+
+print(tupla)
+print(type(tupla))
+print(tupla[2]) #Imprime hasta la posición 2 de los datos de la tupla.
+print(tupla[0:3]) #Debanado de tupla
+print(tupla+tupla2)
