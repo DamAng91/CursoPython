fcc0675b92d6d3baad19a334a64dedcfa60d3e42
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:18:52 -0300

    Create Funciones Matemáticas.py

---
+++ CURSO PYTHON/SECCION 14 - FUNCIONES/14.6 Funciones Matemáticas/Funciones Matemáticas.py
@@ -0,0 +1,14 @@
+import math 
+import random #libreria de métodos aleatorios
+
+print(math.pow(10,2)) #Eleva un numero a una potencia. Tambien se puede utilizar sin la librería math
+print(pow(10,2)) #Tambien se puede utilizar sin la librería math
+print(math.sqrt(64)) #Raiz cuadrada de un número en float
+print(math.isqrt(64)) #Raiz cuadrada de un número en int
+print(math.sin(90)) #Función seno
+print(math.cos(90)) #Función coseno
+print(math.tan(90)) #Función tangente
+print(math.factorial(5)) #Factorial de un numero
+
+print(random.randint(1, 100)) #Elige un numero entero aleatorio entre 1 y 100
+
