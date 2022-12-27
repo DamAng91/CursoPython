8a13ffe079ac1d3591b0b887b6444c1de9a82fcd
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:24:27 -0300

    Create Movimiento de la Tortuga en el lienzo.py

---
+++ CURSO PYTHON/SECCIÓN 25 - INTRODUCCIÓN A TURTLE/25.3 Movimiento de la Tortuga en el lienzo/Movimiento de la Tortuga en el lienzo.py
@@ -0,0 +1,22 @@
+import turtle
+
+s = turtle.Screen()
+t = turtle.Turtle()
+
+t.goto(100,100) #Desplaza la tortuga hacia el punto (100,100)
+t.goto(-100,100)
+# t.goto(0,0) La tortula regresa al punto inicial (0,0)
+t.home() #La tortuga regresa al punto inicial (0,0)
+
+t.fd(100)
+t.rt(90)
+t.fd(100)
+t.rt(90)
+t.fd(100)
+t.rt(90)
+t.fd(100)
+t.rt(90)
+
+
+
+turtle.done()
