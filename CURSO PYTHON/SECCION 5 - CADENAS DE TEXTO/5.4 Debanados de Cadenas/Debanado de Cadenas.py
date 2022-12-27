7e89afd85e9d208d2ae74a99919e4a323476b929
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:23:30 -0300

    Create Debanado de Cadenas.py

---
+++ CURSO PYTHON/SECCION 5 - CADENAS DE TEXTO/5.4 Debanados de Cadenas/Debanado de Cadenas.py
@@ -0,0 +1,9 @@
+#Tambien al debanado de cadenas se lo conoce como sub-strings 
+
+cadena = "Este es un ejemplo de substring (debanado de cadenas)"
+
+#print(len(cadena)) #len devuelve el tamaño de la cadena
+
+print(cadena[2]) #Devuelve el caracter en la posición 2 (t) comenzando desde 0. 
+print(cadena[0:9]) #Devuelve los caracteres desde la posición 0 hasta la posición 9.
+print(cadena[:20]) #Devuelve los caracteres desde la posición 0 hasta la posición 20.
