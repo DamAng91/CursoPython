c5a5f0667a18477d3655bc15c2d3ef7b6320aa04
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:23:25 -0300

    Create Cadenas de Texto.py

---
+++ CURSO PYTHON/SECCION 5 - CADENAS DE TEXTO/5.2 Cadenas de Texto (Práctica)/Cadenas de Texto.py
@@ -0,0 +1,11 @@
+cadena = "Esto es un ejemplo \vde cadena de texto"
+
+print(cadena)
+
+# \"ejemplo\" es una escapada de caracter
+# \n es un salto de linea 
+# \t es una tabulación
+# \b quita es espacio
+#\f símbolo femenino
+# \r elimina lo que antecede al esta notación
+# \v símbolo masculino
