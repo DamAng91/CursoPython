39b0a16422a040e9cb1f691695e02f589036e077
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:24:01 -0300

    Create Metodos Booleanos.py

---
+++ CURSO PYTHON/SECCION 7 - BOOLEANOS/7.5 Métodos Booleanos/Metodos Booleanos.py
@@ -0,0 +1,9 @@
+cadena = "Estoy mostrando los metodos booleanos para las strings"
+cadena2 = "HOLA MUNDO"
+cadena3 = "cadena totalmente en minuscula"
+
+print(cadena.startswith("E")) #Verifica si el método comienza con la "E"
+print(cadena.endswith("s")) #Verifica si la cadena termina con "s"
+
+print(cadena2.isupper()) #Verifica si la cadena está toda en mayúscula.
+print(cadena3.islower()) #Verifica si la cadena está toda en minúscula.
