493a7b0e3f299264e5d16bc8347e455a45288f39
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:17:01 -0300

    Create Llenar una lista vacia.py

---
+++ CURSO PYTHON/SECCION 10 - ESTRUCTURAS DE DATOS - LISTAS/10.6 Llenar una lista vacia/Llenar una lista vacia.py
@@ -0,0 +1,13 @@
+lista = []
+
+edad = int(input("Ingresa tu edad: "))
+lista.append(edad)
+print(lista)
+
+'''lista2 = [4, 5, 6]
+
+lista3 = lista + lista2
+print(lista3)
+
+print("Esta es una lista de distintos datos:", lista)'''
+
