3464c73c03db12f771cfab95699be88ecdcd57be
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:17:20 -0300

    Create Métodos de Diccionarios.py

---
+++ CURSO PYTHON/SECCION 11 - ESTRUCTURAS DE DATOS - DICCIONARIOS/11.3 Métodos de Diccionarios/Métodos de Diccionarios.py
@@ -0,0 +1,15 @@
+diccionario = {1: 2, 2: 3, 3: 4}
+diccionario2 = {4: 5, 6: 7}
+print(diccionario)
+
+#diccionario.pop(3) #Elimina la clave 3
+#diccionario.clear() #Borra todo el diccionario.
+#print(diccionario.get(2)) #Imprime el valor de la clave 2
+#diccionario.setdefault(4, 5) #Crea una nueva clave con su valor.
+#print(diccionario)
+
+#diccionario.update(diccionario2) #Adiciona el diccionario2 a diccionario
+#print(diccionario)
+
+diccionario.copy()
+print(diccionario)
