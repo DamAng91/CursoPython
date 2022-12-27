2b25aadda8232b5c0e22930c9acf447329138f23
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:23:10 -0300

    Create Números.py

---
+++ CURSO PYTHON/SECCION 4 - OPERADORES ARITMETICOS/4.1 Números/Números.py
@@ -0,0 +1,12 @@
+num1 = 40
+num2 = 99.99
+
+print(type(num1))
+print(num1)
+
+print(float(num1)) #Convierte un número cualquiera a flotante 
+
+print(type(num2))
+print(num2)
+
+print(int(num2)) #Convierte un número cualquiera a entero 
