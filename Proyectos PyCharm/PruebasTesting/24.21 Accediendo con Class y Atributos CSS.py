8f7fbe2b764b2b5b77c7a78b7a797457b05a9d1f
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:26:21 -0300

    Create 24.21 Accediendo con Class y Atributos CSS.py

---
+++ Proyectos PyCharm/PruebasTesting/24.21 Accediendo con Class y Atributos CSS.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome(executable_path="Drivers/chromedriver.exe")
+driver.get("https://www.udemy.com/join/login-popup/?skip_suggest=1&locale=es_ES&next=https%3A%2F%2Fwww.udemy.com%2F")
+time.sleep(1)
+
+correo = driver.find_element_by_css_selector("input.form-control")
+clave = driver.find_element_by_css_selector("input.textinput")
+time.sleep(1)
+
+correo.send_keys("damian.angelucci@hotmail.com")
+time.sleep(1)
+
+clave.send_keys("holas231")
+time.sleep(1)
+
+boton = driver.find_element_by_css_selector("input.btn-primary")
+boton.click()
+time.sleep(5)
+
+driver.quit()
