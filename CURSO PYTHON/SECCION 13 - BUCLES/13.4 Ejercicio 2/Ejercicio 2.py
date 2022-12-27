0ee703c041ffbce9fbc716b794f4b2d14faab943
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:18:11 -0300

    Create Ejercicio 2.py

---
+++ CURSO PYTHON/SECCION 13 - BUCLES/13.4 Ejercicio 2/Ejercicio 2.py
@@ -0,0 +1,15 @@
+'''edad = int(input("Ingrese su edad: "))
+
+i = 0
+while i <= edad:
+    print("Los anios cumplidos fueron: ", i)
+    i +=1'''
+
+edad = int(input("Ingrese su edad: "))
+
+i = 1
+
+while i != edad:
+    print("Haz cumplidos: {} anios ".format(i))
+    i +=1
+
