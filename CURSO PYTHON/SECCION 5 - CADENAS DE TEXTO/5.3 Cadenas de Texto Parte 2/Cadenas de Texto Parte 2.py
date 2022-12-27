869eaf85bfbc59e8a19c2ed4f9d3f05f871cb5ec
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:23:28 -0300

    Create Cadenas de Texto Parte 2.py

---
+++ CURSO PYTHON/SECCION 5 - CADENAS DE TEXTO/5.3 Cadenas de Texto Parte 2/Cadenas de Texto Parte 2.py
@@ -0,0 +1,9 @@
+cadena1 = "Hola Walter"
+cadena2 = " Coto"
+numero = 1
+
+print(cadena1+cadena2) #Representa una concatenación de cadenas
+print(cadena2*5) #Repite 5 veces la cadena2
+print("Hola usuario:",cadena2)
+
+print(type(str(numero)))
