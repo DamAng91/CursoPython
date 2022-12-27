40f2b0887544e2f403da9e7a9384cd200d3a3866
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:23:48 -0300

    Create Ejercicio 3.py

---
+++ CURSO PYTHON/SECCION 6 - ENTRADA POR TECLADO Y SALIDA POR PANTALLA}/6.5 Ejercicio 3/Ejercicio 3.py
@@ -0,0 +1,7 @@
+vocal = input("Ingrese una vocal en minuscula: ")
+letra = input("Ingrese una letra en mayuscula: ")
+
+vocal = vocal.upper()
+letra = letra.lower()
+
+print("La vocal es {} y la letra es {}".format(vocal, letra))
