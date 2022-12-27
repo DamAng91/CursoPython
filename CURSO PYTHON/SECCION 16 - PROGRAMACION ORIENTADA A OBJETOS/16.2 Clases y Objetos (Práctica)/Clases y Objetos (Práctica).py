9774b4b9cee689ebc129a936bb1bc29b3ff8f9b4
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:19:08 -0300

    Create Clases y Objetos (Práctica).py

---
+++ CURSO PYTHON/SECCION 16 - PROGRAMACION ORIENTADA A OBJETOS/16.2 Clases y Objetos (Práctica)/Clases y Objetos (Práctica).py
@@ -0,0 +1,10 @@
+class FabricaTelefonos():
+    pass 
+
+print(type(FabricaTelefonos))
+
+celular = FabricaTelefonos() #celular es el objeto creado
+celular2 = FabricaTelefonos()
+
+print(type(celular))
+print(type(celular2))
