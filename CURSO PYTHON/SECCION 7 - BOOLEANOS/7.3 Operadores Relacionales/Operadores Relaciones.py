88017daaf48d2a60ce3f04e92e71ba9987705e6d
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:23:57 -0300

    Create Operadores Relaciones.py

---
+++ CURSO PYTHON/SECCION 7 - BOOLEANOS/7.3 Operadores Relacionales/Operadores Relaciones.py
@@ -0,0 +1,13 @@
+num1 = 1000
+num2 = 500
+cad1 = "La noche esta linda"
+cad2 = "El dia es mas largo que la noche"
+
+print(num1 < num2)
+print(num1 > num2)
+print(num1 >= num2)
+print(num1 <= num2)
+print(num1 != num2)
+print(num1 == num2)
+
+print("Es cad1 menor que cad2?:", cad1 < cad2)
