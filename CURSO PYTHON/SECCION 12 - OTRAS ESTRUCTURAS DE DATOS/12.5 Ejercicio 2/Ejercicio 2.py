32bd4833433c14d29f1e1037ada56ade6c9e9a4d
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:18:01 -0300

    Create Ejercicio 2.py

---
+++ CURSO PYTHON/SECCION 12 - OTRAS ESTRUCTURAS DE DATOS/12.5 Ejercicio 2/Ejercicio 2.py
@@ -0,0 +1,5 @@
+tupla = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+
+numero = int(input("Ingrese un numero del alfabeto: "))
+
+print("La posición elegida corresponde a: ", tupla[numero-1])
