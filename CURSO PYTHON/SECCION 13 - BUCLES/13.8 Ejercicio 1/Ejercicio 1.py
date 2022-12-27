e13a73e46d7010f23e3bde45fe5f2ae2cc8eceb5
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:18:20 -0300

    Create Ejercicio 1.py

---
+++ CURSO PYTHON/SECCION 13 - BUCLES/13.8 Ejercicio 1/Ejercicio 1.py
@@ -0,0 +1,18 @@
+'''lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(lista)
+
+num1 = int(input("Ingrese el primer numero: "))
+num2 = int(input("Ingrese el segundo numero: "))
+
+for i in range(num1, num2):
+    print(i)'''
+
+
+for i in range(1, 11):
+    print(i)
+
+num1 = int(input("Ingrese el primer numero: "))
+num2 = int(input("Ingrese el segundo numero: "))
+
+for i in range(num1, num2+1):
+    print(i)
