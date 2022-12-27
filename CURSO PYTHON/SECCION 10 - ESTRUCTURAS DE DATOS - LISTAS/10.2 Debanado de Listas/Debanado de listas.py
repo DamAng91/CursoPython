723a8d6aa55482f9f2ff228f5e8e30e542c4c234
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:16:37 -0300

    Create Debanado de listas.py

---
+++ CURSO PYTHON/SECCION 10 - ESTRUCTURAS DE DATOS - LISTAS/10.2 Debanado de Listas/Debanado de listas.py
@@ -0,0 +1,8 @@
+lista = [10, 20, 3.14, 'Walter', 'Coto', 7, 'Estudiante', 'Cuaderno']
+
+print(lista[5])
+print(lista[0:5]) #imprime los primeros 5 elementos de la lista.
+print(lista[ :2]) #imprime los primeros 2 elementos de la lista. 
+print(lista[1: ]) #imprime los elementos de la lista sin tener en cuenta al primer elemento.
+print(lista[-1]) #imprime el primer elemento comenzando por el final de la lista
+
