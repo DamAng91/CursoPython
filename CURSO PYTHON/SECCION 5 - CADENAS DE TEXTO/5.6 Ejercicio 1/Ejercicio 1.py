3011d1d9a863586abbfcd1d5c11fe61928338608
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:23:35 -0300

    Create Ejercicio 1.py

---
+++ CURSO PYTHON/SECCION 5 - CADENAS DE TEXTO/5.6 Ejercicio 1/Ejercicio 1.py
@@ -0,0 +1,14 @@
+#Ver consigna en apunte.
+
+cad= "Te quiero solo como amigo"
+
+print(cad[:2])
+
+print(cad[-3:])
+
+print(cad[: :2]) #Lee toda la cadena y luego imprime cada dos caracteres.
+
+print(cad[: : -1]) #Lee toda la cadena y luego imprime a la inversa.
+
+print(cad + cad[: : -1]) #Lee toda la cadena y luego imprime cada dos caracteres.
+
