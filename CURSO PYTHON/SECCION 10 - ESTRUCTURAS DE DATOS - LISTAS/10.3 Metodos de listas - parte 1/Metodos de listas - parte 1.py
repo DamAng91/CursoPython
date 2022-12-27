7ea2eca96ea5a47ddee7986f8356a5053a94ecbf
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:16:40 -0300

    Create Metodos de listas - parte 1.py

---
+++ CURSO PYTHON/SECCION 10 - ESTRUCTURAS DE DATOS - LISTAS/10.3 Metodos de listas - parte 1/Metodos de listas - parte 1.py
@@ -0,0 +1,12 @@
+lista = [1, 2, 3, 4, 5]
+print(lista)
+
+'''lista.append(6)
+print(lista)
+
+lista.append('Python')
+print(lista)'''
+
+lista.insert(2, 2.5) #recibe dos argumentos: el primero, la posición y el segundo, el valor. 
+print(lista)
+
