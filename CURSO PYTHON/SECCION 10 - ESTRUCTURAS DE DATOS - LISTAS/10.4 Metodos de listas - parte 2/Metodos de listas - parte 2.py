9b5132f82e412418fc14978c777ba5c9c4ee3ee7
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:16:51 -0300

    Create Metodos de listas - parte 2.py

---
+++ CURSO PYTHON/SECCION 10 - ESTRUCTURAS DE DATOS - LISTAS/10.4 Metodos de listas - parte 2/Metodos de listas - parte 2.py
@@ -0,0 +1,10 @@
+lista = [5, 3, 1, 4, 2]
+print(lista.count(5)) #permite contar la cantidad de veces que aparece un elemento de la lista
+print(lista.index(4)) #permite reconocer la posición del dato que deseamos buscar.
+
+print(lista)
+lista.sort()
+print(lista) 
+
+lista.reverse()
+print(lista)
