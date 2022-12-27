4e1a7042c144ff5323b5e8ce2f2be56fdbff5ea2
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:26:40 -0300

    Create Link.py

---
+++ Proyectos PyCharm/PruebasTesting/Link.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome(executable_path="Drivers/chromedriver.exe")
+driver.get("https://www.udemy.com/join/login-popup/?locale=en_US&response_type=html&next=https%3A%2F%2Fwww.udemy.com%2F&persist_locale=")
+
+
+link_recuperacion = driver.find_element_by_link_text("Forgot Password")
+link_recuperacion.click()
+
+driver.quit()
