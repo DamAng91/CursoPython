43cdfdbb80d7cce1a7f77d781606bafef78febe7
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:24:09 -0300

    Create Condicionales Anidados.py

---
+++ CURSO PYTHON/SECCION 8 - CONDICIONALES/8.4 Condicionales Anidados/Condicionales Anidados.py
@@ -0,0 +1,10 @@
+nombre = "Juan"
+edad = 18
+
+if nombre == "Juan":
+    if edad >= 18:
+        print("Tu eres Juan y eres mayor de edad")
+    else:
+        print("Eres Juan pero no eres mayor de edad")
+else:
+    print("Tu no eres Juan")
