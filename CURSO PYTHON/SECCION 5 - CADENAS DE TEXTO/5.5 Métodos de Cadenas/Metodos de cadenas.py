74a710634db887133bf230ddd3ffdbbbb51cbcae
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:23:33 -0300

    Create Metodos de cadenas.py

---
+++ CURSO PYTHON/SECCION 5 - CADENAS DE TEXTO/5.5 Métodos de Cadenas/Metodos de cadenas.py
@@ -0,0 +1,9 @@
+cadena = "Estoy utilIzando lOs MetOdos de Python"
+
+print(cadena.lower()) #Convierte todas las mayúsculas en minúsculas.
+print(cadena.upper()) #Convierte todas las minúsculas en mayúsculas.
+print(cadena.capitalize()) #Convierte la primer letra en mayúscula.
+print(cadena.title()) #Convierte todas las iniciales de las palabras en mayúsculas.
+print(cadena.swapcase()) #Convierte las minúsculas en mayúsculas y las mayúsculas en minúsculas.
+
+
